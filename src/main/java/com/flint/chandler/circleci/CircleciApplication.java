9d6f743d7fca17bc1a0958a30bba9933105fcac9
Simon <tmzhangzhe@gmail.com>
2025-01-16 10:36:58 -0700

    add a springboot project

---
+++ src/main/java/com/flint/chandler/circleci/CircleciApplication.java
@@ -0,0 +1,13 @@
+package com.flint.chandler.circleci;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CircleciApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CircleciApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/flint/chandler/circleci/CircleciApplicationTests.java
@@ -0,0 +1,13 @@
+package com.flint.chandler.circleci;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CircleciApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
